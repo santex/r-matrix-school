ce014c7ebbfc68b488722bc2b4dec18e871e3c80
root <root@sante12.(none)>
2012-07-18 04:13:33 +0200

    forgot #!/bin/bash

--- r-batch-plot.sh
+++ r-batch-plot.sh
@@ -1,10 +1,13 @@
- for i in `ls ./backup/*.R`; do 
-cat $i | R --vanilla;  
- n=$(echo $i  | tr -d "./backup/" ); 
- c=$(echo $n  | tr -d "\.R"  );
- echo $i;
- echo $n;
- echo $c;
- sh -c  "mv ./Rplots.pdf  ./plots/"$n".pdf"; 
+#!/bin/bash
+
+for i in `ls ./backup/*.R`;
+do
+cat $i | R --vanilla;
+n=$(echo $i  | tr -d "./backup/" );
+c=$(echo $n  | tr -d "\.R"  );
+echo $i;
+echo $n;
+echo $c;
+sh -c  "mv ./Rplots.pdf  ./plots/"$n".pdf";
 done
 
